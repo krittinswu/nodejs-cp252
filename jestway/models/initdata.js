1b448975085a507a7a17925f46a123111d6351f1
krittinswu <krittin.boonsu@g.swu.ac.th>
2026-03-27 11:15:22 +0700

    Update initdata.js

--- jestway/models/initdata.js
+++ jestway/models/initdata.js
@@ -1,7 +1,7 @@
 // Define some initial data
 const initialUsersData = [
-  //{ email: "alice@example.com", name: 'Alice' },
-  //{ email: "bob@example.com", name: 'Bob' },
+  { email: "alice@example.com", name: 'Alice' },
+  { email: "bob@example.com", name: 'Bob' },
   { email: "coco@example.com", name: 'Coco' }
 ];
 
